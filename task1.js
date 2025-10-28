afa9637fcfc3976f112394923335aea73044cd50
ward <Wardnajaar123465@gmail.com>
2025-10-28 08:26:35 +0200

    Task1

---
+++ task1.js
@@ -0,0 +1,11 @@
+// ורד נגאר 325523017
+// מוחמד ריאן 327640835
+
+let num = 10;
+
+let d2 = num % 2 === 0;
+let d3 = num % 3 === 0;
+let d5 = num % 5 === 0;
+
+let result = d2 + d3 + d5;
+console.log(result);
