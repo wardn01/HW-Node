2122e1368835f6b523640a33bd9269430eb98263
ward <Wardnajaar123465@gmail.com>
2025-11-13 14:23:48 +0200

    Update app.js

--- Task4/app.js
+++ Task4/app.js
@@ -6,11 +6,13 @@ const path = require("path");
 
 const dirPath = path.join(__dirname, "text");
 
-//Random
+//Defines a function getRandomLine() that returns a random letter from the words array.
 function getRandomLine() {
   const words = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k"];
   return words[Math.floor(Math.random() * words.length)];
 }
+
+// Creates 3 files, each containing 7–13 lines, with each line being a random letter from the words array.
 for (let i = 1; i <= 3; i++) {
   let content = "";
   const linesCount = Math.floor(Math.random() * 7) + 7;
@@ -20,7 +22,7 @@ for (let i = 1; i <= 3; i++) {
   fs.writeFileSync(path.join(dirPath, `input${i}.txt`), content.trim());
 }
 
-//Read files
+//Reads the three input files, splits each into lines, and stores them in the `files` array.
 const files = [];
 for (let i = 1; i <= 3; i++) {
   const filePath = path.join(dirPath, `input${i}.txt`);
@@ -28,7 +30,7 @@ for (let i = 1; i <= 3; i++) {
   files.push(content);
 }
 
-// maxLines contains the number of lines in the longest file.
+//Finds the longest file (the one with the most lines).
 let maxLines = 0;
 for (let i = 0; i < files.length; i++) {
   if (files[i].length > maxLines) {
@@ -40,7 +42,7 @@ let finalOutput = [];
 let step = 1;
 let currentLine = 0;
 
-// 1 -> 2 -> 3 -> 4 .....
+//Merges the files by taking 1 line from each, then 2 lines, then 3 lines, and so on, until all lines are added.
 while (currentLine < maxLines) {
   for (let n = 0; n < 3; n++) {
     const lines = files[n].slice(currentLine, currentLine + step);
@@ -50,8 +52,9 @@ while (currentLine < maxLines) {
   step++;
 }
 
-//Output
+//Writes all merged lines to output.txt.
 const outputPath = path.join(dirPath, "output.txt");
 fs.writeFileSync(outputPath, finalOutput.join("\n").trim());
 
 console.log("Done!");
+
