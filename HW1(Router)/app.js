8a5b8fabbd9adf04bd7ac86eeaae06c640bfa608
ward <Wardnajaar123465@gmail.com>
2026-01-02 16:12:52 +0200

    Update app.js

--- HW1(Router)/app.js
+++ HW1(Router)/app.js
@@ -1,5 +1,5 @@
 // ward najjar 325523017
-// mohammed rayan 327640835
+// mohammed ryan 327640835
 // 50/2
 
 const express = require("express");
@@ -30,4 +30,5 @@ app.use((req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 
